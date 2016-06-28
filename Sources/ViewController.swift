4eaa138d69da0c5f47fc10873fc59c0894762ee1
pisarm <flemming@pisarm.dk>
2016-06-28 11:33:22 +0200

    Updated ViewController to use new Timer for driving emulator

--- Sources/ViewController.swift
+++ Sources/ViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var cycleTimer: Timer!
+    var tickTimer: Timer!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         if let gameUrl = NSBundle(forClass: self.dynamicType).URLForResource("Maze", withExtension: "ch8"), data = NSData(contentsOfURL: gameUrl) {
             let rom = Rom(data: data)
             let emulator = Emulator(rom: rom)
 
-            while true {
-                emulator.cycle()
-//                sleep(1)
-            }
+            cycleTimer = Timer(rate: 50) { emulator.cycle() }
+            tickTimer = Timer(rate: 60) { emulator.timerTick() }
+
+            cycleTimer.start()
+            tickTimer.start()
+        } else {
+            print(".ch8 file not found")
         }
     }
 }
