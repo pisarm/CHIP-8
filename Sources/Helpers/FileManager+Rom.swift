e0d738e3641fe61a6f5d8505098679477a155e8f
pisarm <flemming@pisarm.dk>
2016-08-15 22:09:45 +0200

    Tweaked FileManager+Rom to handle name property of Rom

--- Sources/Helpers/FileManager+Rom.swift
+++ Sources/Helpers/FileManager+Rom.swift
@@ -11,12 +11,13 @@ import Foundation
 extension FileManager {
 
     func loadRoms() throws -> [Rom] {
-        let bundleUrl = Bundle(for: self.dynamicType).bundleURL
+        let bundleUrl = Bundle.main.bundleURL
 
         let urls = try contentsOfDirectory(at: bundleUrl, includingPropertiesForKeys: [], options: [])
         let romUrls = urls.filter { $0.pathExtension == "ch8" }
-        let romData = try romUrls.map { try Data(contentsOf: $0) }
-        return romData.map { Rom(data: $0) }
+        let romData = try romUrls.map { ($0.lastPathComponent, try Data(contentsOf: $0)) }
+
+        return romData.map { Rom(name: $0, data: $1) }
     }
 
 }
