3899e04c27631fac11088381cb1b346079d797b5
pisarm <flemming@pisarm.dk>
2016-08-16 14:27:20 +0200

    More Xcode6 beta 8 updates

--- Tests/RawOpcodeTests.swift
+++ Tests/RawOpcodeTests.swift
@@ -11,145 +11,145 @@ import XCTest
 
 final class RawOpcodeTests: XCTestCase {
     //MARK: Setup
-    let test: (rawOpcode: UInt16) -> Void = {
+    let test: (UInt16) -> Void = {
         let opcode = Opcode(rawOpcode: $0)
         XCTAssertEqual(opcode!.rawOpcode, $0)
     }
 
     //MARK: Test
     func testClearScreen() {
-        test(rawOpcode: 0x00E0)
+        test(0x00E0)
     }
 
     func testReturn() {
-        test(rawOpcode: 0x00EE)
+        test(0x00EE)
     }
 
     func testJumpAbsolute() {
-        test(rawOpcode: 0x1111)
+        test(0x1111)
     }
 
     func testCallSubroutine() {
-        test(rawOpcode: 0x2111)
+        test(0x2111)
     }
 
     func testSkipIfEqualValue() {
-        test(rawOpcode: 0x3112)
+        test(0x3112)
     }
 
     func testSkipIfNotEqualValue() {
-        test(rawOpcode: 0x4012)
+        test(0x4012)
     }
 
     func testSkipIfRegisterEqual() {
-        test(rawOpcode: 0x5120)
+        test(0x5120)
     }
 
     func testSetValue() {
-        test(rawOpcode: 0x6722)
+        test(0x6722)
     }
 
     func testAddValue() {
-        test(rawOpcode: 0x7A22)
+        test(0x7A22)
     }
 
     func testSetRegister() {
-        test(rawOpcode: 0x8120)
+        test(0x8120)
     }
 
     func testOrRegister() {
-        test(rawOpcode: 0x8401)
+        test(0x8401)
     }
 
     func testAndRegister() {
-        test(rawOpcode: 0x8402)
+        test(0x8402)
     }
 
     func testXorRegister() {
-        test(rawOpcode: 0x8403)
+        test(0x8403)
     }
 
     func testAddRegister() {
-        test(rawOpcode: 0x8404)
+        test(0x8404)
     }
 
     func testSubtractYFromX() {
-        test(rawOpcode: 0x8405)
+        test(0x8405)
     }
 
     func testShiftRight() {
-        test(rawOpcode: 0x8406)
+        test(0x8406)
     }
 
     func testSubtractXFromY() {
-        test(rawOpcode: 0x8347)
+        test(0x8347)
     }
 
     func testShiftLeft() {
-        test(rawOpcode: 0x8A0E)
+        test(0x8A0E)
     }
 
     func testSkipIfNotEqualRegister() {
-        test(rawOpcode: 0x9120)
+        test(0x9120)
     }
 
     func testSetIndex() {
-        test(rawOpcode: 0xAABC)
+        test(0xAABC)
     }
 
     func testJumpRelative() {
-        test(rawOpcode: 0xBFED)
+        test(0xBFED)
     }
 
     func testAndRandom() {
-        test(rawOpcode: 0xCFAD)
+        test(0xCFAD)
     }
 
     func testDraw() {
-        test(rawOpcode: 0xDEAD)
+        test(0xDEAD)
     }
 
     func testSkipIfKeyPressed() {
-        test(rawOpcode: 0xED9E)
+        test(0xED9E)
     }
 
     func testSkipIfKeyNotPressed() {
-        test(rawOpcode: 0xEAA1)
+        test(0xEAA1)
     }
 
     func testStoreDelayTimer() {
-        test(rawOpcode: 0xFB07)
+        test(0xFB07)
     }
 
     func testStoreKeyPress() {
-        test(rawOpcode: 0xF00A)
+        test(0xF00A)
     }
 
     func testSetDelayTimer() {
-        test(rawOpcode: 0xFA15)
+        test(0xFA15)
     }
 
     func testSetSoundTimer() {
-        test(rawOpcode: 0xFE18)
+        test(0xFE18)
     }
 
     func testAddIndex() {
-        test(rawOpcode: 0xF71E)
+        test(0xF71E)
     }
 
     func testSetIndexFontCharacter() {
-        test(rawOpcode: 0xF729)
+        test(0xF729)
     }
 
     func testStoreBCD() {
-        test(rawOpcode: 0xF533)
+        test(0xF533)
     }
 
     func testWriteMemory() {
-        test(rawOpcode: 0xF555)
+        test(0xF555)
     }
 
     func testReadMemory() {
-        test(rawOpcode: 0xF765)
+        test(0xF765)
     }
 }
